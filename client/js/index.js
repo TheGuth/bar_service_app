2b449518436062fec724edecf495e848480bdec0
Laura Wolfart <laurawolfart@gmail.com>
2017-03-19 18:40:07 -0400

    react initial file structure

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,16 @@
 import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(
+        <Provider store={store}>
+          
+        </Provider>,
+        document.getElementById('app')
+    )
+);
