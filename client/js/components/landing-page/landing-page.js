3699024007b1becff1266f66e37155c9d613e43c
James Guthrie <jamesrguthrie@gmail.com>
2017-03-22 12:52:05 -0400

    added styling to landing page

--- client/js/components/landing-page/landing-page.js
+++ client/js/components/landing-page/landing-page.js
@@ -11,14 +11,23 @@ export class LandingPage extends React.Component {
     render() {
       return (
         <div className="landing-page-container">
-          <Link to="/login"><button>Log In</button></Link>
-          <h1>Bar Service App</h1>
-          <h2>Connect to a Business!</h2>
-          // add value to input to grab for params in Link
-          <input type="text" placeholder="Unique ID:" value={this.props.userIdInput} onChange={(e) => this.props.dispatch(actions.proccessUserIdInput(e.target.value))}/>
-          <Link to={{pathname: '/client/dashboard', params: {id: this.props.userIdInput}} }><button>Connect</button></Link>
-          <h3>Business Sign Up</h3>
-          <Link to="/signup"><button>Sign Up</button></Link>
+          <div className="d-flex flex-column align-items-end">
+            <Link to="/login"><button className="landing-page-login btn btn-primary btn-block">Log In</button></Link>
+          </div>
+          <div className="d-flex flex-column align-items-center landing-page-title">
+            <h1>Bar Service App</h1>
+          </div>
+          <div className="d-flex flex-column align-items-center landing-page-connect">
+            <h2>Connect to a Business!</h2>
+            <input type="text" placeholder="Unique ID:" className="landing-page-connect-input" value={this.props.userIdInput} onChange={(e) => this.props.dispatch(actions.proccessUserIdInput(e.target.value))}/>
+            <Link to={{pathname: '/client/dashboard', params: {id: this.props.userIdInput}} }><button className="btn btn-primary btn-lg btn-block">Connect</button></Link>
+          </div>
+          <div className="d-flex flex-column align-items-center landing-page-signup">
+            <h3>Business Sign Up</h3>
+            <div>
+              <Link to="/signup"><button className="btn btn-primary btn-lg btn-block">Sign Up</button></Link>
+            </div>
+          </div>
         </div>
       )}
   }
