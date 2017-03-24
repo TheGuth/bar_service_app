1051d56dbc98a882e31e051f40aa938492e42eac
James Guthrie <jamesrguthrie@gmail.com>
2017-03-24 15:03:17 -0400

    edit config file

---
+++ server/config.js
@@ -0,0 +1,11 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       'mongodb://localhost/bar_app';
+
+// mongodb://james:pass55@ds137040.mlab.com:37040/bar_app
+
+exports.TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+	'mongodb://localhost/test_bar_app');
+
+exports.PORT = process.env.PORT || 8080;
